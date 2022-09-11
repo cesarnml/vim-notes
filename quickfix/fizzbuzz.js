7934b9bdd8b502a469ab70e941a2b02a76f03cb0
Cesar Napoleon Mejia <cmejia@gmail.com>
2022-09-11 14:50:16 +0700

    Interim

--- quickfix/fizzbuzz.js
+++ quickfix/fizzbuzz.js
@@ -9,7 +9,8 @@
 var i;
 for (i=1; i <= 100; i++) {
     if(i % 15 == 0) {
-        console.log('Fizzbuzz');
+        const newLocal = 'Fizzbuzz';
+        console.log(newLocal);
     } else if(i % 5 == 0) {
         console.log('Buzz');
     } else if(i % 3 == 0) {
